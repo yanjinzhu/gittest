adc10bffe70c02f6811721be008ea859375a811a
yanjinzhu <wy18235626@163.com>
2018-06-15 22:00:27 +0800

    lkfjdslfj;sa

--- myselenium/run_all_case.py
+++ myselenium/run_all_case.py
@@ -1,8 +1,8 @@
 import unittest,HTMLTestRunner
 import os
 
-case_path = "D:\\jenkins\\workspace\\selenium_ui\\myselenium\\case\\"
-report_path = "D:\jenkins\workspace\selenium_ui\myselenium\report"
+case_path = "D:\jenkins\workspace\selenium_ui\myselenium\case"
+report_path = "D:\\jenkins\\workspace\\selenium_ui\\myselenium\\report\\"
 
 def all_case():
     disccover = unittest.defaultTestLoader.discover(case_path,pattern="test*.py",top_level_dir=None)
@@ -12,7 +12,7 @@ def all_case():
 if __name__ == "__main__":
     #runner = unittest.TextTestRunner()
     #runner.run(all_case())
-    report_abspath = "D:\jenkins\workspace\selenium_ui\myselenium\report\result.html"
+    report_abspath = os.path.join(report_path,"result.html")
     f = open(report_abspath,"wb")
     runner = HTMLTestRunner.HTMLTestRunner(stream=f,title="UI自动化测试报告,测试结果如下：",description="用例执行情况：")
 
